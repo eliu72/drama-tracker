15de27284d5b9c6758ad4b49417a3041c74bf8f7
Elaine Liu <eliu72@uwo.ca>
2021-10-10 13:39:44 -0400

    -patch: add status code checks, notion page schema, linting

--- drama_tracker/app.py
+++ drama_tracker/app.py
@@ -15,6 +15,7 @@ MOVIE_DB_BASE_URL = "https://api.themoviedb.org/3/"
 def get_title_details(
     title: str = "", content_type: str = "", language: str = "en", year: str = ""
 ) -> Dict:
+    # query search endpoint
     query_url = (
         MOVIE_DB_BASE_URL
         + f"search/{content_type}?api_key={MOVIE_DB_API_KEY}"
@@ -22,38 +23,70 @@ def get_title_details(
     )
     response = requests.get(query_url)
     data = response.json()
-    id = data["results"][0]["id"]
+
+    # check return status
+    if response.status_code != 200:
+        raise Exception(
+            "Title Search Error "
+            + str(response.status_code)
+            + ": "
+            + str(response.reason)
+        )
 
     # get full details using id
+    id = data["results"][0]["id"]
     query_url = MOVIE_DB_BASE_URL + f"{content_type}/{id}?api_key={MOVIE_DB_API_KEY}"
     response = requests.get(query_url)
+
+    # check return status
+    if response.status_code != 200:
+        raise Exception(
+            "Get Title Details Error "
+            + str(response.status_code)
+            + ": "
+            + str(response.reason)
+        )
+
     return response.json()
 
 
-def get_main_actors(id: str = "", content_type: str = "") -> List[str]:
+def get_main_actors(movie_id: str = "", content_type: str = "") -> List[str]:
+    # query credits endpoint
     query_url = (
-        MOVIE_DB_BASE_URL + f"{content_type}/{id}/credits?api_key={MOVIE_DB_API_KEY}"
+        MOVIE_DB_BASE_URL
+        + f"{content_type}/{movie_id}/credits?api_key={MOVIE_DB_API_KEY}&language=en"
     )
     response = requests.get(query_url)
+
+    # check return status
+    if response.status_code != 200:
+        raise Exception(
+            "Get Main Actors Error "
+            + str(response.status_code)
+            + ": "
+            + str(response.reason)
+        )
+
     data = response.json()
-    cast_list = data[:5]
+    cast_list = data["cast"][:5]
     return cast_list
 
 
 def patch_notion_db_item(page_details: JSON = None) -> None:
-    try:
-        # get details of new database page item
-        page_details = json.loads(page_details)
-        page_id = page_details["id"]
-        content_type = page_details["properties"]["Content Type"][0]["text"]
-        title = page_details["properties"]["Title"][0]["text"]
-        language = page_details["properties"]["Language"]["name"]
 
+    # extract details from new notion databse item
+    page_details = json.loads(page_details)
+    page_id = page_details["id"]
+    content_type = page_details["properties"]["Content Type"][0]["text"]
+    title = page_details["properties"]["Title"][0]["text"]
+    language = page_details["properties"]["Language"]["name"]
+
+    try:
         # get details of the specified title
         title_details = get_title_details(title, content_type, language)
         cast_list = get_main_actors(title_details["id"], content_type)
-    except Exception:
-        logging.info("Error accessing Notion page or Movies DB.")
+    except Exception as e:
+        logging.info(e)
 
     # create patch_request
     patch_request = helpers.patch_all(
@@ -80,4 +113,12 @@ def patch_notion_db_item(page_details: JSON = None) -> None:
         "Notion-Version": "2021-08-16",
     }
     response = requests.patch(notion_base_url, headers=NOTION_HEADER)
-    response
+
+    # check status code
+    if response.status_code != 200:
+        logging.error(
+            "Notion API Error "
+            + str(response.status_code)
+            + ": "
+            + str(response.reason)
+        )
